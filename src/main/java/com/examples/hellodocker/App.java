6d44c9d0f94e3319a7b776fc0d9bc50b28baad74
cortom31@gmx.com <cortom31@gmx.com>
2022-05-07 16:17:47 +0200

    add maven build with Docker + add yaml workflows for linux, macos, and windows

--- src/main/java/com/examples/hellodocker/App.java
+++ src/main/java/com/examples/hellodocker/App.java
@@ -3,7 +3,15 @@ package com.examples.hellodocker;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hola por Docker ;) ");
+		
+		System.out.println("Hello World!");
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
